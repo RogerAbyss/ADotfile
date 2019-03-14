9eef83809b0d8f74dc0e8bbfdd791009ed482d7d
abyss <roger_ren@qq.com>
2019-03-14 17:35:45 +0800

    no message

--- auto_installer.rb
+++ auto_installer.rb
@@ -58,8 +58,7 @@ puts "\033[32m \n
 gemList = YAML.load_file(File.dirname(__FILE__)+'/config/gem.yml')
 if gemList.length > 0
     puts "\033[32m rvm 安装 \033[0m"
-    system "brew install gnupg"
-    system "curl -sSL https://rvm.io/mpapis.asc | gpg --import -"
+    system "gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
     system "curl -sSL https://get.rvm.io | bash -s stable"
     system "rvm use default"
     puts "\033[32m gem 升级 \033[0m"
