7b78af53dee5b1d736f7501903e47fb922e52c36
abyss <roger_ren@qq.com>
2019-03-15 09:35:55 +0800

    no message

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,8 @@
 
 if [ -f ~/ADotfile/auto_installer.rb ]
 then
-    cd ~/ADotfile&&/usr/bin/git pull&&/usr/bin/ruby ./auto_installer.rb
+    cd ~/ADotfile&&/usr/bin/git pull
+    /usr/bin/ruby ~/ADotfile/auto_installer.rb
 else
     /usr/bin/git clone https://github.com/RogerAbyss/ADotfile.git ~/ADotfile
     cd ~/ADotfile&&sh bootstrap.sh
